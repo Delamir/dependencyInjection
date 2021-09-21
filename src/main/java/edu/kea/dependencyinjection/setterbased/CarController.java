1e6c468da98fa361aa6bc092b80d4c0aff26b3db
Kringelum2 <p.kringelum@gmail.com>
2021-09-21 14:07:58 +0200

    bitch ass joachim bitch

---
+++ src/main/java/edu/kea/dependencyinjection/setterbased/CarController.java
@@ -0,0 +1,27 @@
+package edu.kea.dependencyinjection.setterbased;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+
+
+    private CarService c;
+
+    @Autowired
+    public void setC(CarService c) {
+        this.c = c;
+    }
+
+    @GetMapping("/ligemeget")
+    public String getCars() {
+        return c.getCars();
+    }
+
+
+
+}

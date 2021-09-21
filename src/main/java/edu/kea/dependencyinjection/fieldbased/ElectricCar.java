e733094c1795340811e93e0ad3a906d1dfffc68e
Fladbrod <73465595+Fladbrod@users.noreply.github.com>
2021-09-21 14:10:50 +0200

    hej

--- src/main/java/edu/kea/dependencyinjection/fieldbased/ElectricCar.java
+++ src/main/java/edu/kea/dependencyinjection/fieldbased/ElectricCar.java
@@ -1,28 +1,12 @@
 package edu.kea.dependencyinjection.fieldbased;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class ElectricCar {
 
-    @Autowired
-    String type;
-    @Autowired
-    boolean turbo;
-    @Autowired
-    String brand;
+    @Bean
+    public
 
-
-    public ElectricCar(String type, boolean turbo, String brand) {
-        this.type = type;
-        this.turbo = turbo;
-        this.brand = brand;
-
-        System.out.println(type + turbo + brand);
-    }
-
-    public ElectricCar() {
-
-    }
 }

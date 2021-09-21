d0d3aef27de12cad124b0e2b75d9459625723422
Delamir <71318139+Delamir@users.noreply.github.com>
2021-09-21 13:30:03 +0200

    Push

--- src/main/java/edu/kea/dependencyinjection/constructorbased/Car.java
+++ src/main/java/edu/kea/dependencyinjection/constructorbased/Car.java
@@ -1,4 +1,4 @@
-package edu.kea.dependencyinjection.models;
+package edu.kea.dependencyinjection.constructorbased;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
